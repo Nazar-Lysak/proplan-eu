aab0c812da73718188684e98eab096908b9af1a8
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2024-08-30 22:46:08 +0300

    init comit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `proplan-eu`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
